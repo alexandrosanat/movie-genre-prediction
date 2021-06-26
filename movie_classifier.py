495884f64f9f515b201e535b8d2ff185e827d9fa
alexandrosanat <alexandrosanat@gmail.com>
2021-06-26 14:18:36 +0100

    First working version of app

--- movie_classifier.py
+++ movie_classifier.py
@@ -1,6 +1,7 @@
 import typer
 import text_processing as tp
 import joblib
+import numpy as np
 
 
 # Create a new typer app
@@ -10,11 +11,14 @@ app = typer.Typer()
 # Called when the app is loaded
 def load_model():
     # Get the path to the trained model file and load it
-    model_path = "./movie_genre_classifier.pkl"
+    model_path = "./movie_classifier_trained_model.pkl"
 
     with open(model_path, 'rb') as filename:
-        model = joblib.load(filename)
-    return model
+        loaded_data = joblib.load(filename)
+        model = loaded_data['pipeline']
+        genres = loaded_data['genres']
+
+    return model, genres
 
 
 # Called when a request is received
@@ -26,22 +30,18 @@ def predict(title: str, description: str):
     """
     # Pre-process user input
     pre_processed_text = tp.transform_text(title, description)
-    vectorised_text = tp.vectorise_text(pre_processed_text)
     # Load the model
-    model = load_model()
+    model, genres = load_model()
     # Get a prediction from the model
-    genre_array = model.predict(vectorised_text)
-
-    vectoriser_path = "./movie_genre_vectoriser.pkl"
-    with open(vectoriser_path, 'rb') as filename:
-        vectoriser = joblib.load(filename)
-
-    genre = vectoriser.inverse_transform(genre_array)
+    probabilities = model.predict_proba([pre_processed_text])
+    threshold = 0.5
+    top_genres = np.where(probabilities > threshold, 1, 0)
+    prediction = list(genres[top_genres[0] > 0])
 
     # Return the prediction
     output = {"title": title,
               "description": description,
-              "genre": genre}
+              "genre": prediction}
     return output
 
 
