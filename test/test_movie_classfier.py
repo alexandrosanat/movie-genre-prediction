568196ae394f4920930fd37f4827e743be577338
alexandrosanat <alexandrosanat@gmail.com>
2021-06-26 19:31:55 +0100

    Updating model

--- test/test_movie_classfier.py
+++ test/test_movie_classfier.py
@@ -4,6 +4,7 @@ utf-8
 """
 import unittest
 import movie_classifier
+import text_processing as tp
 import json
 
 
@@ -22,6 +23,14 @@ class TestMovieClassifier(unittest.TestCase):
     def setUpClass(cls):
         cls.pickle = movie_classifier.load_model()
 
+        cls.test_input = {
+            'title': 'Avengers',
+            'description': 'Earths mightiest heroes must come together and learn to fight as a '
+                           'team if they are going to stop the mischievous Loki and his alien '
+                           'army from enslaving humanity.',
+            'threshold': 0.5
+            }
+
     ###############
     # Tests ####
     ###############
@@ -38,17 +47,21 @@ class TestMovieClassifier(unittest.TestCase):
         pipeline = self.pickle.get('pipeline')
         self.assertEqual([step[0] for step in pipeline.steps], ['Vectorizer', 'Classifier'])
 
+    def test_text_processing(self):
+        """
+        Test that for given input the pre-processed text matches the expected.
+        """
+        title = self.test_input.get('title')
+        description = self.test_input.get('description')
+        expected_output = "avenger earth mightiest hero must come together learn fight " \
+                          "team going stop mischievous loki alien army enslaving humanity"
+        self.assertEqual(tp.transform_text(title, description), expected_output)
+
     def test_movie_classifier(self):
         """
         Test that for given input the output is as expected.
         """
-        test_input = {
-            'title': 'Avengers',
-            'description': 'Earths mightiest heroes must come together and learn to fight as a '
-                           'team if they are going to stop the mischievous Loki and his alien '
-                           'army from enslaving humanity.',
-            'threshold': 0.5
-        }
+        test_input = self.test_input
 
         output = json.loads(movie_classifier.main(test_input.get('title'),
                                                   test_input.get('description'),
